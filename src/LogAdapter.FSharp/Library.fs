9b1d3c668c30d0840821e224562ffc788b74872a
Oskar Gewalli <oskar@gewalli.se>
2018-10-21 18:54:15 +0200

    Enum for level

--- src/LogAdapter.FSharp/Library.fs
+++ src/LogAdapter.FSharp/Library.fs
@@ -2,17 +2,17 @@ namespace LogAdapter.FSharp
 
 module Logger=
     /// Log level
-    module Level=
-        let error =5
-        let warn =4
-        let info = 3
-        let debug = 2
-        let verbose = 1
-    type LogMessage = int * string * exn * obj
+    type Level=
+        |Error =5
+        |Warn =4
+        |Info = 3
+        |Debug = 2
+        |Verbose = 1
+    type LogMessage = Level * string * exn * obj
     type Logger = LogMessage -> unit
     
-    let log (logger:Logger) s values = logger (Level.debug, s, null, values)
+    let log (logger:Logger) s values = logger (Level.Debug, s, null, values)
     let logf (logger:Logger) formatString values= Printf.kprintf (log logger) formatString values
     
-    let logError (logger:Logger) exn s values = logger (Level.error, s, exn, values)
+    let logError (logger:Logger) exn s values = logger (Level.Error, s, exn, values)
     let logErrorf (logger:Logger) exn formatString values= Printf.kprintf (log logger) exn formatString values
